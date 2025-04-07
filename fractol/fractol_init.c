3ce0ea56b571c14799b0c8b5d790a7e0f51f25ae
Kharbachzoubair <zoubairkharbach@gmail.com>
2025-04-07 22:30:54 +0100

    done

--- fractol/fractol_init.c
+++ fractol/fractol_init.c
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fractol.h>
+
+void    *factal_init(t_fractal *fractal)
+{
+    fractal->mlx_connection=mlx_init();
+    if(fractal->mlx_connection==NULL)
+    {
+        malloc_error();
+    }
+    fractal->mlx_window = mlx_new_window(fractal->mlx_connection,
+                                        WIDTH,
+                                        HEIGHT,
+                                        fractal->name);
+    if(fractal->mlx_window==NULL)
+    {
+        mlx_destroy_display(fractal->mlx_window,fractal->mlx_connection);
+        free(fractal->mlx_connection);
+        malloc_error();
+    }
+}
